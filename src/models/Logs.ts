1483e483dc62e85aa21b7f67ac34292af39b2767
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-06-28 11:33:48 +0800

    Added desc field to log model

--- src/models/Logs.ts
+++ src/models/Logs.ts
@@ -10,6 +10,7 @@ export interface ILog {
   newData?: string;
   objectId: string;
   unicode?: string;
+  description?: string;
 }
 
 export interface ILogDocument extends ILog, Document {}
@@ -18,9 +19,6 @@ export interface ILogModel extends Model<ILogDocument> {
   createLog(doc: ILog): Promise<ILogDocument>;
 }
 
-/**
- * When oldData field is empty, it means all fields have been changed.
- */
 export const schema = new Schema({
   _id: field({ pkey: true }),
   createdAt: field({
@@ -40,16 +38,15 @@ export const schema = new Schema({
   oldData: field({ type: String, label: 'Unchanged field names', optional: true }),
   newData: field({ type: String, label: 'Changed field names', optional: true }),
   objectId: field({ type: String, label: 'Collection row id' }),
-  unicode: field({ type: String, label: 'Description' }),
+  unicode: field({ type: String, label: 'Performer username' }),
+  description: field({ type: String, label: 'Description' }),
 });
 
 export const loadLogClass = () => {
   class Log {
     public static createLog(doc: ILog) {
       const { oldData, newData } = doc;
-      const logDoc = {
-        ...doc,
-      };
+      const logDoc = { ...doc };
 
       if (doc.action === 'update' && oldData && newData) {
         const comparison = getChangedFields(JSON.parse(oldData), JSON.parse(newData));
